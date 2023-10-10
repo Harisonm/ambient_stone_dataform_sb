d397c03d2a6c58f17e021967197811db10856324
Manitra Ranaivoharison <manitra.ranaivoharison@converteo.com>
2023-10-10 22:59:32 +0200

    change function

--- includes/dataformUtils.js
+++ includes/dataformUtils.js
@@ -1,17 +1,17 @@
 const { dataform } = require('@dataform/core');
 
-function loadDataFromBucket(bucket_name, uri, dataset, table) {
+function loadDataFromBucket(bucket_name, uri, dataset, table, config) {
+  console.log(`${config}`);
   return `
     LOAD DATA OVERWRITE \`${dataset}.${table}\` (
       id INT64,
       shop_name STRING
     )
-    CLUSTER BY shop_name
     FROM FILES (
       format = 'JSON',
       uris = ['${bucket_name}/${uri}\']
     );
   `;
 }
-
+  
 module.exports = { loadDataFromBucket };
